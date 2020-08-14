f2fcfa06a283673c8b04f5c0513a98145a0bb1c8
bdjeffyp <bdjeffyp@users.noreply.github.io>
2020-08-14 15:20:14 -0700

    Updates

---
+++ precache-manifest.08a5971b92e171ac37abda8cf23f4931.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d93af59b8c1ae79e69f465b3963e0fe",
+    "url": "/ffr-tracker/index.html"
+  },
+  {
+    "revision": "80f8e1861830dd421078",
+    "url": "/ffr-tracker/static/css/main.ca3bd040.chunk.css"
+  },
+  {
+    "revision": "a1b6e65c58e86974f289",
+    "url": "/ffr-tracker/static/js/2.09a65b43.chunk.js"
+  },
+  {
+    "revision": "2206f6f6cbbf99cae08d01b2952f71ad",
+    "url": "/ffr-tracker/static/js/2.09a65b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f8e1861830dd421078",
+    "url": "/ffr-tracker/static/js/main.e8ba9c0d.chunk.js"
+  },
+  {
+    "revision": "c88748d8d753a2bc2765",
+    "url": "/ffr-tracker/static/js/runtime-main.8f2852b3.js"
+  },
+  {
+    "revision": "92650de0c80248dd6bfab73ced766bc6",
+    "url": "/ffr-tracker/static/media/grid.92650de0.png"
+  }
+]);
